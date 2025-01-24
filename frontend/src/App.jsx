6cac1aa45e5ae162c6d952edc3976192098cffce
Aashna-Verma <aashna.verma@outlook.com>
2025-01-24 15:12:34 -0500

    feat(frontend): add page routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from '@/components/ui/button'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ReportForm from "./pages/ReportForm";
+import ModifyReport from "./pages/ModifyReport";
+import NotifyMe from "./pages/NotifyMe";
+import NoPage from "./pages/NoPage";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-4xl'>Vite + React</h1>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<Home />} />
+				<Route path="report/create" element={<ReportForm />} />
+				<Route path="report/modify/:id" element={<ModifyReport />} />
+				<Route path="notifyMe" element={<NotifyMe />} />
+				<Route path="*" element={<NoPage />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
