271f438951024a2d57638fbe7c3cb403a8b1665d
Aashna-Verma <aashna.verma@outlook.com>
2025-01-24 18:30:59 -0500

    update things

--- frontend/src/pages/NoPage.jsx
+++ frontend/src/pages/NoPage.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import NavDock from "@/components/ui/nav-dock";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 
 const NoPage = () => {
-	return <></>;
+	return (
+		<>
+			<NavDock />
+			<Card className="m-10 sm:m-20">
+				<CardHeader>
+					<CardTitle className="text-2xl">404 there is nothing here!!</CardTitle>
+				</CardHeader>
+
+				<CardFooter className="flex justify-between">
+					<a href="/">
+						<Button>Go Home</Button>
+					</a>
+				</CardFooter>
+			</Card>
+		</>
+	);
 };
 
 export default NoPage;
--- frontend/src/pages/NotifyMe.jsx
+++ frontend/src/pages/NotifyMe.jsx
@@ -1,24 +1,9 @@
 import React from "react";
 
 import { Button } from "@/components/ui/button";
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
-import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
-} from "@/components/ui/select";
 import { PhoneInput } from "@/components/ui/phone-input";
 import NavDock from "@/components/ui/nav-dock";
 
