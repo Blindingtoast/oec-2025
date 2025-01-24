a2cb25afaffe6a719dfffc19f1f184ae6f67ca99
Aashna-Verma <aashna.verma@outlook.com>
2025-01-24 15:21:03 -0500

    feat(frontend): add pages for each section

---
+++ frontend/src/pages/ModifyReport.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ModifyReport = () => {
+	return <div>ModifyReport</div>;
+};
+
+export default ModifyReport;
---
+++ frontend/src/pages/NotifyMe.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotifyMe = () => {
+	return <div>NotifyMe</div>;
+};
+
+export default NotifyMe;
