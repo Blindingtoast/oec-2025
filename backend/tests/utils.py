b6788cc8419ae33165a31bdb3de7b803fc070bae
Angus Jull <angusjull1@gmail.com>
2025-01-25 14:01:16 -0500

    add more tests

---
+++ backend/tests/utils.py
@@ -0,0 +1,36 @@
+import random
+import datetime
+from backend.database.models import ReportSchema, UserSchema
+
+
+def generate_report(lat, lon, deviation, types):
+    """Create a report to be sent by a client, with a random name
+
+    Args:
+        lat (float): a latitude coordinate
+        lon (float): a longitude coordinate
+        radius (float): the distance (in kilometers) from the coordinates that a report can be generated
+        type (list[ReportType]): the types of reports that can be generated
+    """
+    # Conver deviation from km to degrees
+    deg_dev = deviation / 111.0
+    rand_lat = random.gauss(lat, deg_dev)
+    rand_lon = random.gauss(lon, deg_dev)
+    reporttime = datetime.datetime.now().isoformat()
+    report = ReportSchema(
+        lat=rand_lat,
+        long=rand_lon,
+        type=random.choice(types),
+        time=reporttime,
+        description="This is a test report",
+    )
+    return report
+
+
+def generate_user(lat, lon, deviation):
+    rand_lat = random.gauss(lat, deviation)
+    rand_lon = random.gauss(lon, deviation)
+    email = "testemail@email.com"
+    phone = "123-456-7890"
+    user = UserSchema(lat=rand_lat, long=rand_lon, email=email, phone=phone)
+    return user
