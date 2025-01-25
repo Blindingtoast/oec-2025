f7df0b3cdc4e7ae330c248612e57f41eb9d9b16a
Aashna-Verma <aashna.verma@outlook.com>
2025-01-24 19:37:27 -0500

    map

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,45 @@
 import React from "react";
 import NavDock from "@/components/ui/nav-dock";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+	const [center, setCenter] = useState(null);
+	const [markerPositions, setMarkerPositions] = useState([]);
+
+	useEffect(() => {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(success, error);
+		} else {
+			console.log("Geolocation not supported");
+		}
+
+		// get json from api/reports/location and set markerPositions
+		fetch("/api/reports/locations")
+			.then((response) => response.json())
+			.then((data) => {
+				setMarkerPositions(data.map((report) => ({ lat: report.lat, lng: report.long })));
+			});
+
+		console.log("Fetching locations", markerPositions);
+	}, []);
+
+	function success(position) {
+		const latitude = position.coords.latitude;
+		const longitude = position.coords.longitude;
+		setCenter({ lat: latitude, lng: longitude });
+		console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+	}
+
+	function error() {
+		console.log("Unable to retrieve your location");
+	}
+
 	return (
 		<>
 			<NavDock />
 			<div className="flex justify-center items-center">
-				<Map />
+				<Map center={center} markerPositions={markerPositions} />
 			</div>
 		</>
 	);
@@ -18,12 +50,7 @@ const containerStyle = {
 	height: "90vh",
 };
 
-const center = {
-	lat: -3.745,
-	lng: -38.523,
-};
-
-function Map() {
+function Map({ center, markerPositions }) {
 	const { isLoaded } = useJsApiLoader({
 		id: "google-map-script",
 		googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
@@ -32,7 +59,6 @@ function Map() {
 	const [map, setMap] = React.useState(null);
 
 	const onLoad = React.useCallback(function callback(map) {
-		// This is just an example of getting and using the map instance!!! don't just blindly copy!
 		const bounds = new window.google.maps.LatLngBounds(center);
 		map.fitBounds(bounds);
 
@@ -51,8 +77,9 @@ function Map() {
 			onLoad={onLoad}
 			onUnmount={onUnmount}
 		>
-			{/* Child components, such as markers, info windows, etc. */}
-			<></>
+			{markerPositions.map((position, index) => (
+				<Marker key={index} position={position} />
+			))}
 		</GoogleMap>
 	) : (
 		<></>
